1caa604f5981f39af34b627d491b5e51fd50b129
vrindashine1 <vrindashine2507@gmail.com>
2025-12-31 19:08:53 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,9 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/admin", adminRoutes);
 
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
 app.listen(process.env.PORT, () =>
   console.log(`Server running on port ${process.env.PORT}`)
 );
